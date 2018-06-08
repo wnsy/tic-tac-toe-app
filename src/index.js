835e9f518f3464e13a929d139ff2b4d8f81e191c
Ayesha W <ayesha.wnz@gmail.com>
2018-06-08 16:25:42 +1200

    Changes the Square render method to display the value from the current state and to toggle it on click

--- src/index.js
+++ src/index.js
@@ -10,9 +10,13 @@ class Square extends React.Component {
     };
   }
   render() {
-    return (
-      <button className="square" onClick={() => alert('click')}>
-        {this.props.value}
+    return ( // change to display the val. fr. the curr. state & to toggle it on click
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
+        { /*whenever this.setState is called, an update to
+          the component is scheduled causing React to merge in the passed state
+          update and re-render the component along w/ its decendants,
+          this.state.value will be 'X', so you'll see an 'X' in the grid. */ }
       </button>
     );
   }
